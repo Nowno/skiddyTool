733dcf9419fa0055f39802cbea3d3ee2cedfd960
Nowno <33899422+Nowno@users.noreply.github.com>
2018-05-24 02:03:57 +0200

    Add files via upload

---
+++ APPPP/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FormSkin1 = New APPPP.FormSkin()
+        Me.FlatButton1 = New APPPP.FlatButton()
+        Me.FlatButton2 = New APPPP.FlatButton()
+        Me.FlatButton3 = New APPPP.FlatButton()
+        Me.FlatTextBox1 = New APPPP.FlatTextBox()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatButton3)
+        Me.FormSkin1.Controls.Add(Me.FlatButton2)
+        Me.FormSkin1.Controls.Add(Me.FlatButton1)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(209, 130)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "         SkiddySkid"
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(5, 53)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(66, 31)
+        Me.FlatButton1.TabIndex = 0
+        Me.FlatButton1.Text = "Import"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton2
+        '
+        Me.FlatButton2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton2.BaseColor = System.Drawing.Color.Green
+        Me.FlatButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.FlatButton2.Location = New System.Drawing.Point(5, 87)
+        Me.FlatButton2.Name = "FlatButton2"
+        Me.FlatButton2.Rounded = False
+        Me.FlatButton2.Size = New System.Drawing.Size(66, 31)
+        Me.FlatButton2.TabIndex = 1
+        Me.FlatButton2.Text = "RecycleBin"
+        Me.FlatButton2.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton3
+        '
+        Me.FlatButton3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton3.BaseColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.FlatButton3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton3.Location = New System.Drawing.Point(77, 73)
+        Me.FlatButton3.Name = "FlatButton3"
+        Me.FlatButton3.Rounded = False
+        Me.FlatButton3.Size = New System.Drawing.Size(131, 31)
+        Me.FlatButton3.TabIndex = 2
+        Me.FlatButton3.Text = "Start CTRL + F"
+        Me.FlatButton3.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatTextBox1
+        '
+        Me.FlatTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox1.Location = New System.Drawing.Point(26, 55)
+        Me.FlatTextBox1.MaxLength = 32767
+        Me.FlatTextBox1.Multiline = False
+        Me.FlatTextBox1.Name = "FlatTextBox1"
+        Me.FlatTextBox1.ReadOnly = True
+        Me.FlatTextBox1.Size = New System.Drawing.Size(10, 29)
+        Me.FlatTextBox1.TabIndex = 3
+        Me.FlatTextBox1.Text = "FlatTextBox1"
+        Me.FlatTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.FlatTextBox1.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox1.UseSystemPasswordChar = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(209, 130)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FormSkin1 As FormSkin
+    Friend WithEvents FlatButton1 As FlatButton
+    Friend WithEvents FlatButton3 As FlatButton
+    Friend WithEvents FlatButton2 As FlatButton
+    Friend WithEvents FlatTextBox1 As FlatTextBox
+End Class
---
+++ APPPP/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System
+Imports System.IO
+Public Class Form1
+    Private Sub FlatButton1_Click(sender As Object, e As EventArgs) Handles FlatButton1.Click
+        Dim myfiledlog As New OpenFileDialog
+        myfiledlog.InitialDirectory = "C:\"
+        myfiledlog.Filter = "TextFiles *.txt*|*.txt*"
+        myfiledlog.FilterIndex = 2
+        myfiledlog.RestoreDirectory = True
+        If myfiledlog.ShowDialog() =
+               DialogResult.OK Then
+                If Dir(myfiledlog.FileName) <> "" Then
+                    MsgBox("Imported " & myfiledlog.FileName)
+
+                Else
+                    MsgBox("File Not found ", vbCritical)
+
+                End If
+            End If
+            FlatTextBox1.Text = myfiledlog.FileName
+
+    End Sub
+
+    Private Sub FlatButton2_Click(sender As Object, e As EventArgs) Handles FlatButton2.Click
+        Dim path As String = "C:\$Recycle.Bin"
+        Dim dt As DateTime = Directory.GetLastWriteTime(path)
+        MsgBox(dt)
+    End Sub
+
+    Private Sub FlatButton3_Click(sender As Object, e As EventArgs) Handles FlatButton3.Click
+        If FlatTextBox1.Text = "" Then
+            MsgBox("Please select a file")
+
+        Else
+            Dim findstring = IO.File.ReadAllText(FlatTextBox1.Text)
+            If findstring.Contains("d[<N6") Then
+                MsgBox("Found wetnoodle", MsgBoxStyle.Critical)
+            End If
+            If findstring.Contains("avatarsmokeweedeveryday420") Then
+                MsgBox("Found magneta", MsgBoxStyle.Critical)
+            Else
+                MsgBox("Clean if no error")
+            End If
+        End If
+
+    End Sub
+End Class
